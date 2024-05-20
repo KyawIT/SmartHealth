48ab9cda054663da778834965e749dff57b5b2ba
MladenovicNikola <n.mladenovic@students.htl-leonding.ac.at>
2024-05-20 19:27:52 +0200

    Privacy Policy Update + Navbar Update + Dark/Light Switch done

--- SmartHealth_Frontend/src/routes/auth/signup/+page.svelte
+++ SmartHealth_Frontend/src/routes/auth/signup/+page.svelte
@@ -1,51 +1,53 @@
 <div id="container" class="mt-5">
-  <h1
-    class="my-5 text-6xl font-black text-blue-600 dark:text-blue-600 text-center"
-  >
-    SIGNUP
-  </h1>
-  <form class="max-w-sm mx-auto">
-    <div class="mb-5">
-      <label
-        for="base-input"
-        class="block mb-2 text-sm font-medium text-gray-900">Email</label
-      >
-      <input
-        type="text"
-        id="base-input"
-        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-      />
-    </div>
-
-    <div class="mb-5">
-      <label
-        for="base-input"
-        class="block mb-2 text-sm font-medium text-gray-900">Password</label
-      >
-      <input
-        type="password"
-        id="base-input"
-        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-      />
-    </div>
-
-    <div class="mb-5">
-      <label
-        for="base-input"
-        class="block mb-2 text-sm font-medium text-gray-900">SVN-Nr</label
-      >
-      <input
-        type="text"
-        id="base-input"
-        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-      />
-    </div>
-    <button
-      type="submit"
-      class="mb-5 w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >Register</button
+  <div class="rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 w-1/2 m-auto pb-2">
+    <h1
+      class="my-5 text-6xl font-black text-blue-600 dark:text-blue-600 text-center"
     >
-  </form>
+      SIGNUP
+    </h1>
+    <form class="max-w-sm mx-auto">
+      <div class="mb-5">
+        <label
+          for="base-input"
+          class="block mb-2 text-sm font-medium text-white">Email</label
+        >
+        <input
+          type="text"
+          id="base-input"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        />
+      </div>
+
+      <div class="mb-5">
+        <label
+          for="base-input"
+          class="block mb-2 text-sm font-medium text-white">Password</label
+        >
+        <input
+          type="password"
+          id="base-input"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        />
+      </div>
+
+      <div class="mb-5">
+        <label
+          for="base-input"
+          class="block mb-2 text-sm font-medium text-white">SVN-Nr</label
+        >
+        <input
+          type="text"
+          id="base-input"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        />
+      </div>
+      <button
+        type="submit"
+        class="mb-5 w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >Register</button
+      >
+    </form>
+    </div>
 </div>
 
 <style>
